ba9db21636695e9d4e66ec53637b28b1ade7ee91
SOTIRIOS SVERONIS <62189294+sotiris08@users.noreply.github.com>
2021-12-30 15:49:11 +0200

    Μια πρώτη αρχή για την Client
    Δεν τελείωσα.

---
+++ Client.py
@@ -0,0 +1,6 @@
+from Gui import Gui as Gui
+
+class Client:
+    Gui = Gui
+    FoodData = None
+    ProfileData = None
---
+++ Gui.py
@@ -0,0 +1,10 @@
+class Gui:
+    ProfileView = None
+    StartView = None
+    PersonInfoView = None
+    CalendarView = None
+    FoodSearchView = None
+
+    def getActiveView(): pass
+    def destroyActiveView(): pass
+    def beginView(view): pass
