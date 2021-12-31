0ee387111e0fb906c8d1b1aca3287ad40194e250
SOTIRIOS SVERONIS <62189294+sotiris08@users.noreply.github.com>
2021-12-31 13:44:04 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,7 +1,8 @@
 from Gui import Gui as Gui
 from ProfileData import ProfileData as ProfileData
+from FoodData import FoodData as FoodData
 
 class Client:
     Gui = Gui
-    FoodData = None
+    FoodData = FoodData
     ProfileData = ProfileData
