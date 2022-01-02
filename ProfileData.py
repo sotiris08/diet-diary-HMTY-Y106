f8b7aa7e8027d455f7abfe8c6d63b77d2d31a466
SOTIRIOS SVERONIS <62189294+sotiris08@users.noreply.github.com>
2022-01-02 19:30:37 +0200

    Διόρθωση Σφαλμάτων

--- ProfileData.py
+++ ProfileData.py
@@ -7,11 +7,15 @@ class ProfileData:
     weightData = None    #WeightHistory
     hydrationData = None  #HydrationHistory
     bmiData = None    #BMIHistory
+    calpDay = None
     height = None
     
     def init():
         f = open("./data/height.txt", "r", encoding="utf-8")
         ProfileData.height = f.read()
+        f.close()
+        f = open("./data/calpday.txt", "r", encoding="utf-8")
+        ProfileData.calpDay = f.read()
         f.close()
         ProfileData.load_profile_data()
         
@@ -34,11 +38,19 @@ class ProfileData:
             f.write(f"{new_height}")
         ProfileData.height = new_height
 
+    def change_calpDay(new_calpDay: int):
+        with open("./data/calpday.txt", "w", encoding="utf-8") as f:
+            f.write(f"{new_calpDay}")
+        ProfileData.calpDay = new_calpDay
+
     def get_height():
         return ProfileData.height
 
+    def get_calpDay():
+        return ProfileData.calpDay
+
     def isFirstTime():
-        if ProfileData.weightData == None and ProfileData.hydrationData == None and ProfileData.bmiData == None:
+        if (not ProfileData.weightData.hasData()) and (not ProfileData.hydrationData.hasData()) and (not ProfileData.bmiData.hasData()):
             return True
         else:
             return False
--- ProfileDataClasses.py
+++ ProfileDataClasses.py
@@ -6,6 +6,10 @@ class WeightHistory:
     def __init__(self, data):   #data => το λεξικό από το αρχείο json
         self.history = data
 
+    def hasData(self):
+        if len(self.history) == 0: return False
+        else: return True
+
     def getWeightForDays(self, dates):
         '''Δίνεις τις ημέρες που θες σε μορφή λίστας και σου επιστρέφει ένα λεξικό με κλειδιά της ημέρες και για τιμές το βάρος'''
         result = {}
@@ -28,6 +32,7 @@ class WeightHistory:
         except KeyError: raise DateNotFoundError('Invalid Date')
 
     def saveWeightHistory(data):
+        data = getValidJSON(data)
         f = open('./data/weightData.json', "w", encoding='utf-8')
         f.write(data)
         f.close()
@@ -39,6 +44,10 @@ class HydrationHistory:
 
     def __init__(self, data):  #data => το λεξικό από το αρχείο json
         self.history = data
+
+    def hasData(self):
+        if len(self.history) == 0: return False
+        else: return True
 
     def getHydrationForDays(self, dates):
         '''Δίνεις τις ημέρες που θες σε μορφή λίστας και σου επιστρέφει ένα λεξικό με κλειδιά της ημέρες και για τιμές το hydration'''
@@ -62,6 +71,7 @@ class HydrationHistory:
         except KeyError: raise DateNotFoundError('Invalid Date')
 
     def saveHydrationHistory(data):
+        data = getValidJSON(data)
         f = open('./data/hydrationData.json', 'w', encoding='utf-8')
         f.write(data)
         f.close()
@@ -74,6 +84,10 @@ class BMIHistory:
 
     def __init__(self, data):  #data => το λεξικό από το αρχείο json
         self.history = data
+
+    def hasData(self):
+        if len(self.history) == 0: return False
+        else: return True
 
     def getBMIForDays(self, dates):
         '''Δίνεις τις ημέρες που θες σε μορφή λίστας και σου επιστρέφει ένα λεξικό με κλειδιά της ημέρες και για τιμές το BMI'''
@@ -97,9 +111,18 @@ class BMIHistory:
         except KeyError: raise DateNotFoundError('Invalid Date')
 
     def saveBMIHistory(data):
+        data = getValidJSON(data)
         f = open('./data/bmiData.json', 'w', encoding='utf-8')
         f.write(data)
         f.close()
 
     #def __str__(self):
     #    return f"{self.date},{self.bmi}"
+
+def getValidJSON(data):
+    data = str(data)
+    i = 0
+    while i <= len(data):
+        data = data.replace("'",'"')
+        i += 1
+    return data
